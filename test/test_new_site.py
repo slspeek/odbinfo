87036e433f7b2f36be32244018226a78094e1a20
Steven L. Speek <slspeek@gmail.com>
2021-01-05 21:42:04 +0100

    first attempt to use travis-ci

--- test/test_new_site.py
+++ test/test_new_site.py
@@ -1,15 +1,32 @@
 """ test site creation """
 import os
-from resource import TEST_OUTPUT
+import logging
+from test.connect import datasource, start_office
+from test.resource import TEST_OUTPUT, DEFAULT_TESTDB
+from pytest import fixture
 from odbinfo.writer import create_jekyll_site, _make_site
 from odbinfo.reader import read_tables
-from connect import datasource
+
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.DEBUG)
 
 
-def test_new_site():
+# pylint:disable=unused-argument
+def test_new_site(libreoffice):  # pylint:disable=redefined-outer-name
     """ test new site scaffolding """
     result = create_jekyll_site(TEST_OUTPUT.format(""), "test-site")
     assert result == os.path.join(TEST_OUTPUT.format(""), "test-site")
+
+
+@fixture(scope="module")
+def libreoffice():
+    """ A libreoffice running on a test repository """
+    testdb = os.getenv("BD_TESTDB", DEFAULT_TESTDB)
+    office_proc = start_office(testdb)
+    yield office_proc
+    office_proc.terminate()
+    logger.debug("LibreOffice killed")
 
 
 def test_generate_tables_site():

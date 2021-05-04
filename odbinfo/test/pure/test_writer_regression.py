03d1b84b5bb99884972bca5e4a561ad45b3c0f77
Steven L. Speek <slspeek@gmail.com>
2021-05-05 01:02:38 +0200

    no regression test on hugo generated site

--- odbinfo/test/pure/test_writer_regression.py
+++ odbinfo/test/pure/test_writer_regression.py
@@ -13,23 +13,24 @@ from odbinfo.test.resource import FIXTURE_DIR_TPL, TEST_OUTPUT_TPL
 def test_quick_view(metadata_processed):
     """ Run without database scan """
     name = "testdb"
-    make_site_regression_test(name, metadata_processed, FIXTURE_DIR_TPL.format(
-        "writer_fixtures/testdb-local"))
+    make_site_regression_test(name, metadata_processed,
+                              FIXTURE_DIR_TPL.format("writer_fixtures/testdb"))
 
 
 @pytest.mark.slow
 def test_quick_view_empty(empty_metadata_processed):
     """ Run without database scan """
     name = "emptydb"
-    make_site_regression_test(name, empty_metadata_processed, FIXTURE_DIR_TPL.format(
-        "writer_fixtures/emptydb-local"))
+    make_site_regression_test(name, empty_metadata_processed,
+                              FIXTURE_DIR_TPL.format("writer_fixtures/emptydb"))
 
 
-def make_site_regression_test(name, metadata, fixture_name_local: str):
+def make_site_regression_test(name, metadata, fixture_name: str):
     " generate report and verify"
     outdir = TEST_OUTPUT_TPL.format("")
     site_dir_local = make_site(outdir, name, metadata)
     site_dir = site_dir_local[:-6]
-    fixture_name = fixture_name_local[:-6]
-    assert os.system(f"diff -r {fixture_name} {site_dir}") == 0
-    
+    assert os.system(f"diff -r {fixture_name}/content {site_dir}/content") == 0
+
+    assert os.system(f"diff -r {fixture_name}/config.toml"
+                     f" {site_dir}/config.toml") == 0

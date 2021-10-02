f3c3b55272eeb7bf53ba9eefbe4a3d493df79798
Steven L. Speek <slspeek@gmail.com>
2021-10-02 23:52:24 +0200

    test: searchexec.py 100% coverage

--- odbinfo/test/pure/dependency/test_searchexec.py
+++ odbinfo/test/pure/dependency/test_searchexec.py
@@ -1,7 +1,13 @@
 " test dependency search in basicfunctions "
-from odbinfo.pure.dependency.searchexec import (consider,
+import unittest
+
+from odbinfo.pure.datatype import (Identifier, Library, Module, WebPage,
+                                   content_type)
+from odbinfo.pure.dependency.searchexec import (_link_name_tokens, consider,
+                                                rewrite_module_callable_links,
                                                 search_callable_in_callable)
 from odbinfo.pure.parser.basic import get_basic_tokens, scan_basic
+from odbinfo.pure.processor import _process_library
 
 SOURCE_MODULEONE = """
 Sub Foo()
@@ -25,6 +31,12 @@ End Sub
 
 Sub Bar()
 End Sub
+"""
+
+SOURCE_FUNCTION = """
+Function Answer()
+    Answer() = 42
+End Function
 """
 
 
@@ -71,3 +83,51 @@ def test_consider_other_lib():
     assert len([token for token in mod1_foo_sub.tokens if token.link]) == 0
     consider(mod2_wose_sub, mod1_foo_sub)
     assert len([token for token in mod2_wose_sub.tokens if token.link]) == 1
+
+
+def test_remove_recursive_calls():
+    "remove_recursive_calls"
+    callables = _scan_basic(SOURCE_FUNCTION, "Standard", "ModuleOne")
+    search_callable_in_callable(callables)
+
+
+class ModuleOneTest(unittest.TestCase):
+    " a processed Module "
+
+    def setUp(self):
+        self.module = Module("Module", "Lib", SOURCE_MODULEONE)
+        lib = Library("Lib", [self.module])
+        _process_library(lib)
+        search_callable_in_callable(self.module.callables)
+
+
+class RewriteModuleLinksTest(ModuleOneTest):
+    "rewrite_module_callable_links"
+
+    def test_rewrite_module_callable_links(self):
+        "only basicfunction links"
+        rewrite_module_callable_links([self.module])
+        linkindex = self.module.callables[0].calls[0].name_token.index
+        token = self.module.tokens[linkindex]
+        identifier = Identifier(content_type(
+            Module), "Module.Lib", "Bar")
+        assert token.link == identifier
+
+    def test_rewrite_module_callable_links_with_skip(self):
+        "other link"
+        self.module.callables[-1].tokens[-1].link = WebPage("foo").identifier
+        rewrite_module_callable_links([self.module])
+        # other links untouched
+        assert self.module.callables[-1].tokens[-1].link == WebPage(
+            "foo").identifier
+
+
+class LinkNameTokens(ModuleOneTest):
+    "_link_name_tokens"
+
+    def test_link_name_tokens(self):
+        "_link_name_tokens"
+        _link_name_tokens(self.module)
+        func = self.module.callables[0]
+        name_token = self.module.tokens[func.name_token_index]
+        assert name_token.link == func.identifier

2ebb133781b5fdc248996182f0fa4ae95f55cfd3
Steven L. Speek <slspeek@gmail.com>
2021-12-23 21:24:48 +0100

    got expensive call to pyenv out of Makefile into env.source

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ set -e
 git submodule update --init --recursive
 (cd pipenvconf/oo && PIPENV_IGNORE_VIRTUALENVS=1 pipenv install -d)
 (cd pipenvconf/pure && PIPENV_IGNORE_VIRTUALENVS=1 pipenv install -d)
+source ./env.source
 ODBINFO_NO_BROWSE=1 make travis
 ODBINFO_NO_BROWSE=1 make clean coverage
 make metric mypy

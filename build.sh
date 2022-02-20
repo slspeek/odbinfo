9fdefa765d148e954d4f85f0acba6b9fbffaa8b5
Steven L. Speek <slspeek@gmail.com>
2022-02-20 22:27:39 +0100

    travis build 3

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -o errexit
 source ./env.source
 (cd pipenvconf/oo && PIPENV_IGNORE_VIRTUALENVS=1 pipenv run make -C ../..   travis)
-(cd pipenvconf/pure &&  ODBINFO_NOBROWSE=1 PIPENV_IGNORE_VIRTUALENVS=1 pipenv run make -C ../.. clean coverage)
+(cd pipenvconf/pure &&  ODBINFO_NO_BROWSE=1 PIPENV_IGNORE_VIRTUALENVS=1 pipenv run make -C ../.. clean coverage)
 (cd pipenvconf/pure &&  PIPENV_IGNORE_VIRTUALENVS=1 pipenv run make -C ../.. metric mypy)

58059e81ac24e600c85936b259c95009280cb462
Steven L. Speek <slspeek@gmail.com>
2022-03-05 23:47:16 +0100

    checkout.sh cleans up afterwards

--- bash/checkout.sh
+++ bash/checkout.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
 REPO=$(pwd)
-cd $(mktemp -d)
+TEMPDIR=$(mktemp -d)
+cd $TEMPDIR
 git clone $REPO
 cd odbinfo
 git submodule update --init --recursive
 bash/install-venvs.sh
 bash/build.sh
+(cd pipenvconf/oo && pipenv --rm)
+(cd pipenvconf/pure && pipenv --rm)
+cd $REPO
+rm -rf $TEMPDIR
